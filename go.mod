b5d3f1a6f35def3ce2c140598f4f17bb4c636995
thottel64 <105885349+thottel64@users.noreply.github.com>
2023-02-07 11:58:07 -0600

    init commmit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thottel64/golang_madlibs
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	if descriptor := r.FormValue("descriptor"); descriptor != "" {
+		log.Printf("Descriptor: %s", r.FormValue("descriptor"))
+	}
+	if animal1 := r.FormValue("animal1"); animal1 != "" {
+		log.Printf("Animal1: %s", r.FormValue("animal1"))
+	}
+	if verb := r.FormValue("verb"); verb != "" {
+		log.Printf("Verb: %s", r.FormValue("verb"))
+	}
+	if adverb := r.FormValue("adverb"); adverb != "" {
+		log.Printf("Adverb: %s", r.FormValue("adjective"))
+	}
+	if adjective := r.FormValue("adjective"); adjective != "" {
+		log.Printf("Adjective: %s", r.FormValue("adjective"))
+	}
+	if noun := r.FormValue("noun"); noun != "" {
+		log.Printf("noun: %s", r.FormValue("noun"))
+	}
+	err := t.templ.Execute(w, r)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "index.html"})
+	http.Handle("/greet", &templateHandler{filename: "templates/greeting.html"})
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
